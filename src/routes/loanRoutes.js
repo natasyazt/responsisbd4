c6d5de6585e9d707cf8804554b740e9c28de35f3
natasyazt <natasyaztt@gmail.com>
2026-04-28 13:45:20 +0700

    Update loanRoutes.js

--- src/routes/loanRoutes.js
+++ src/routes/loanRoutes.js
@@ -3,6 +3,7 @@ import { LoanController } from '../controllers/loanController.js';
 
 const router = express.Router();
 
+router.get('/top-borrowers', LoanController.getTopBorrowers); // Route baru
 router.get('/', LoanController.getLoans);
 router.post('/', LoanController.createLoan);
 
